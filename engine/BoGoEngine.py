6907635ef944271926163b26a76194e373cb94d3
Long T. Dam <longdt90@gmail.com>
2012-09-03 12:27:21 +0700

    Change the source and file names to follow the conventions

--- engine/BoGoEngine.py
+++ engine/BoGoEngine.py
@@ -3,19 +3,19 @@
 # Copyright (c) 2012- Long T. Dam <longdt90@gmail.com>,
 #                     Trung Ngo <ndtrung4419@gmail.com>
 #
-# This file is part of IBus-Bogo Project
+# This file is part of IBus-BoGo Project
 # IBus-Bogo is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# IBus-Bogo is distributed in the hope that it will be useful,
+# IBus-BoGo is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with IBus-Bogo.  If not, see <http://www.gnu.org/licenses/>.
+# along with IBus-BoGo.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import GObject
 from gi.repository import IBus
@@ -52,7 +52,7 @@ class Engine(IBus.Engine):
         if not is_press:
             return False
 
-        if self.isCharacter(keyval):
+        if self.is_character(keyval):
             if state & (modifier.CONTROL_MASK | modifier.MOD1_MASK) == 0:
                 print "Character entered: " + chr(keyval)
                 self.isFakeBackspace = True
@@ -64,19 +64,19 @@ class Engine(IBus.Engine):
                 #     self.commitPreedit()
                 #     return True
                 self.OldString = self.NewString
-                self.processChar(keyval)
+                self.process_char(keyval)
                 print "Old string:", self.OldString
                 print "New string:", self.NewString
                 self.nBackspace, self.StringToCommit = \
-                  self.getFakeBackspace_StringToCommit()
+                  self.get_nbackspace_and_string_to_commit()
                 print "String to commit:", self.StringToCommit
-                self.commitFakeBackspace()
+                self.commit_fake_backspace()
                 return True
 
         if self.StringToCommit:
             if keyval == keysyms.Return or keyval == keysyms.Escape or\
                keyval == keysyms.space:
-                self.resetEngine()
+                self.reset_engine()
                 return False
 
             if keyval == keysyms.BackSpace:
@@ -85,34 +85,34 @@ class Engine(IBus.Engine):
                     self.nBackspace -= 1
                     if self.nBackspace == 0:
                         print "Last fake backspace. Commit..."
-                        self.commitResult()
+                        self.commit_result()
                         self.isFakeBackspace = False
                         return True
                 else:
                     print "A real backspace"
-                    self.removeLastChar()
+                    self.remove_last_char()
                 return False
 
-        self.resetEngine()
+        self.reset_engine()
         return False
 
-    def resetEngine(self):
+    def reset_engine(self):
         self.StringToCommit = u""
         self.NewString = u""
         self.OldString = u""
         self.isFakeBackspace = False
         self.nBackspace = 0
 
-    def commitResult(self):
+    def commit_result(self):
         self.commit_text(IBus.Text.new_from_string(self.StringToCommit))
 
-    def processChar(self, char):
+    def process_char(self, char):
         self.NewString += unichr(char)
 
-    def removeLastChar(self):
+    def remove_last_char(self):
         self.NewString = self.NewString[:-1]
 
-    def commitFakeBackspace(self):
+    def commit_fake_backspace(self):
         global dpy, BG_BACKSPACE
         print "Commit "+ str(self.nBackspace) + " fake Backspaces"
         number = self.nBackspace #Silly Python
@@ -121,7 +121,7 @@ class Engine(IBus.Engine):
             Xtst.XTestFakeKeyEvent(dpy, BG_BACKSPACE, False, 0)
             Xlib.XFlush(dpy)
 
-    def getFakeBackspace_StringToCommit(self):
+    def get_nbackspace_and_string_to_commit(self):
         length = len(self.OldString)
         if length == 0:
             return 1, self.NewString
@@ -132,11 +132,11 @@ class Engine(IBus.Engine):
                 return _nbackspace, _stringtocommit
         return 1, self.NewString[length:]
 
-    def isCharacter(self, keyval):
+    def is_character(self, keyval):
         if keyval in xrange(33,126):
             return True
         else:
             return False
 
-    def isEndingCharacter(self, keyval):
+    def is_ending_character(self, keyval):
         pass
--- engine/BoGoMain.py
+++ engine/BoGoMain.py
@@ -1,21 +1,21 @@
-# IBus-Bogo - The Vietnamese IME for IBus
+# IBus-BoGo - The Vietnamese IME for IBus
 #
 # Copyright (c) 2012- Long T. Dam <longdt90@gmail.com>,
 #                     Trung Ngo <ndtrung4419@gmail.com>
 #
-# This file is part of IBus-Bogo Project
-# IBus-Bogo is free software: you can redistribute it and/or modify
+# This file is part of IBus-BoGo Project
+# IBus-BoGo is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# IBus-Bogo is distributed in the hope that it will be useful,
+# IBus-BoGo is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with IBus-Bogo.  If not, see <http://www.gnu.org/licenses/>.
+# along with IBus-BoGo.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import IBus
 from gi.repository import GLib
