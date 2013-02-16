bd3c1c041f4a8768bd067287dd74a7302c23767c
Trung Ngo <ndtrung4419@gmail.com>
2013-02-16 16:30:23 +0700

    New Vietnamese translation

--- config-gui/locales/vi_VN.ts
+++ config-gui/locales/vi_VN.ts
@@ -5,77 +5,72 @@
     <message>
         <location filename="controller.ui" line="45"/>
         <source>General</source>
-        <translation>Chung</translation>
+        <translation>Thiết lập chung</translation>
     </message>
     <message>
         <location filename="controller.ui" line="56"/>
         <source>Input &amp;Method:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Kiểu gõ:</translation>
     </message>
     <message>
         <location filename="controller.ui" line="69"/>
         <source>&amp;Output Charset:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Bảng mã:</translation>
     </message>
     <message>
         <location filename="controller.ui" line="122"/>
         <source>GUI Language:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngôn ngữ giao diện: </translation>
     </message>
     <message>
         <location filename="controller.ui" line="141"/>
         <source>Spellchecking</source>
-        <translation type="unfinished"></translation>
+        <translation>Chính tả</translation>
     </message>
     <message>
         <location filename="controller.ui" line="150"/>
         <source>Skip non-Vietnamese:</source>
-        <translation type="unfinished"></translation>
+        <translation>Bỏ qua những từ không đúng tiếng Việt: </translation>
     </message>
     <message>
         <location filename="controller.ui" line="161"/>
         <source>Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Công cụ</translation>
     </message>
     <message>
         <location filename="controller.ui" line="167"/>
         <source>Charset converter</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ chuyển đổi bảng mã</translation>
     </message>
     <message>
         <location filename="controller.ui" line="179"/>
         <source>Copy some text, choose the source encoding and the destination encoding and press &quot;Convert&quot;. Then paste back.</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy một đoạn văn bản, chọn bảng mã nguồn, bảng mã đích. Ấn nút &quot;Chuyển đổi&quot; và paste trở lại.</translation>
     </message>
     <message encoding="UTF-8">
         <location filename="controller.ui" line="203"/>
         <source>→</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">→</translation>
     </message>
     <message>
         <location filename="controller.ui" line="226"/>
         <source>Convert</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuyển đổi</translation>
     </message>
     <message>
         <location filename="controller.ui" line="273"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Trợ giúp</translation>
     </message>
     <message>
         <location filename="controller.ui" line="280"/>
         <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation>Khôi phục thiết lập chuẩn</translation>
     </message>
     <message>
-        <location filename="controller.ui" line="307"/>
+        <location filename="controller.ui" line="300"/>
         <source>Close</source>
         <translation>Đóng</translation>
-    </message>
-    <message>
-        <location filename="controller.ui" line="287"/>
-        <source>PushButton</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -86,14 +81,14 @@
         <translation type="obsolete">Đóng</translation>
     </message>
     <message>
-        <location filename="controller.py" line="127"/>
+        <location filename="controller.py" line="128"/>
         <source>Vietnamese</source>
         <translation>Tiếng Việt</translation>
     </message>
     <message>
-        <location filename="controller.py" line="128"/>
+        <location filename="controller.py" line="129"/>
         <source>US English</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếng Anh Mỹ</translation>
     </message>
 </context>
 </TS>
