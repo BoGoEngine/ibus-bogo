74745a6ea03200097d93a673e8819a76d5e9fd94
Trung Ngo <ndtrung4419@gmail.com>
2014-01-11 20:38:46 +0700

    Make BaseConfig use fallback values in default_configs.json. Fix #141

---
+++ ibus_engine/test/test_config.py
@@ -0,0 +1,34 @@
+from nose.tools import eq_
+from base_config import BaseConfig
+import tempfile
+import os
+
+
+class TestBaseConfig():
+
+    def setup(self):
+        self.config_file = \
+                tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    
+    def teardown(self):
+        self.config_file.close()
+        os.remove(self.config_file.name)
+
+    def test_empty_file(self):
+        # If the file is empty then BaseConfig should write
+        # some default value in there
+        config = BaseConfig(path=self.config_file.name)
+        content = self.config_file.file.read()
+
+        assert len(content) != 0
+
+    def test_file_missing_key(self):
+        # BaseConfig should use default_config.json to
+        # fill in missing config keys from the user config file
+        
+        self.config_file.file.write("{}")
+        self.config_file.file.flush()
+        config = BaseConfig(path=self.config_file.name)
+
+        # Should not raise an exception here
+        config["auto-capitalize-expansion"]
