d4ec7ca1cb61ab0334e2388223a6af9c56d18eea
NgoHuy <huynhok.uit@gmail.com>
2014-01-12 14:42:18 +0700

    Add test toUnikeyRules method

--- ibus_engine/test/test_abbreviation.py
+++ ibus_engine/test/test_abbreviation.py
@@ -78,3 +78,6 @@ vn:Việt Nam\
         eq_(self.abbr.expand("kg"), "không")
         eq_(self.abbr.expand("kgcd"), "Không Gian Cộng Đồng")
         eq_(self.abbr.expand("vn"), "Việt Nam")
+
+    def  test_toUnikeyRules(self):
+        self.abbr.toUnikeyRules()
