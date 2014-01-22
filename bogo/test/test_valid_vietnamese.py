ed9cc10c606426606b9ce302d6408f8e3ef02dbe
Trung Ngo <ndtrung4419@gmail.com>
2014-01-22 22:20:44 +0700

    Add more manual tests for Vietnamese validation

--- bogo/test/test_valid_vietnamese.py
+++ bogo/test/test_valid_vietnamese.py
@@ -28,3 +28,25 @@ def test_c_ending():
 
     assert is_valid_string("ic", final_form=True) is False
     assert is_valid_string("ic", final_form=False) is True
+
+
+def test_ch_ending():
+    assert is_valid_string("ếch", final_form=True)
+    assert is_valid_string("ách", final_form=True)
+    assert is_valid_string("ích", final_form=True)
+
+    assert is_valid_string("ech", final_form=True) is False
+
+    assert is_valid_string("ìch", final_form=True) is False
+    assert is_valid_string("ãch", final_form=True) is False
+
+
+def test_non_final():
+    assert is_valid_string("thuyen", final_form=False)
+    assert is_valid_string("thuyen", final_form=True) is False
+
+    assert is_valid_string("thuyeen", final_form=False) is False
+    assert is_valid_string("thuyeeng", final_form=False) is False
+
+    assert is_valid_string("ảch", final_form=False) is True
+    assert is_valid_string("ảch", final_form=True) is False
