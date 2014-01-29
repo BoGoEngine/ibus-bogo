9195be708df21a418651894c70b0694de61d48b9
Trung Ngo <ndtrung4419@gmail.com>
2014-01-29 22:56:28 +0700

    Abstract the backspace-oriented part of ibus_engine.py into BackspaceBackend

--- ibus_engine/ibus_engine.py
+++ ibus_engine/ibus_engine.py
@@ -44,32 +44,164 @@ import vncharsets
 vncharsets.init()
 
 
+class BackspaceBackend:
+
+    def __init__(self, engine):
+        self.engine = engine
+        self.can_do_surrounding_text = False
+        self.ever_checked_surrounding_text = False
+
+        self.focus_tracker = FocusTracker()
+
+    def reset(self):
+        self.first_time_sending_backspace = True
+
+    def update_composition(self, string):
+        self.commit_string(string)
+
+    def commit_composition(self):
+        self.commit_string(self.engine.new_string)
+
+    def commit_string(self, string):
+        same_initial_chars = list(takewhile(lambda tupl: tupl[0] == tupl[1],
+                                            zip(self.engine.prev_string,
+                                                string)))
+
+        n_backspace = len(self.engine.prev_string) - len(same_initial_chars)
+        string_to_commit = string[len(same_initial_chars):]
+
+        self.delete_prev_chars(n_backspace)
+
+        # Charset conversion
+        # We encode a Unicode string into a byte sequence with the specified
+        # encoding and then re-interpret it as a Unicode string containing
+        # only latin-1 (ISO-8859-1) characters.
+        #
+        # This method has the flaw of not being able to commit raw legacy
+        # encoding strings but it's the only thing we can do now as we cannot
+        # commit bytes in Python 3, just Unicode string. Consider this example:
+        #
+        #     - UTF-8 'à': 0xc3 0xa0
+        #     - TCVN3 'à': 0xb5 (rendered as '¶')
+        #     - UTF-8 '¶': 0xc2 0xb6
+        #
+        # So if we typed 'à' in TCVN3, what the client receives would be
+        # 0xc2 0xb6. Field testing shows that this does not affect LibreOffice
+        # Writer and Kate (when forcing the file's encoding to be latin-1)
+        # though.
+        if self.engine.config['output-charset'] != 'utf-8':
+            string_to_commit = string_to_commit \
+                .encode(self.engine.config['output-charset']) \
+                .decode('latin-1')
+
+        # We cannot commit the text in Gtk since there is a bug in which
+        # sometimes committed text comes before the forwarded
+        # backspaces, resulting in undesirable output. Instead, we forward
+        # each character to the current input context.
+        #
+        # We also need to detect whether the current input context is from a
+        # GTK app by checking for the SURROUNDING_TEXT capability. It works
+        # because as of right now (2013/02/22, IBus 1.4.1), only the IBus Gtk
+        # client can do surrounding text.
+        #
+        # Very very veryyyy CRUDE, by the way.
+        if self.engine.input_context_capabilities & \
+                IBus.Capabilite.SURROUNDING_TEXT:
+            logging.debug("Forwarding as commit...")
+
+            for ch in string_to_commit:
+                keyval = mapping[ch] if ch in mapping else ord(ch)
+                self.engine.forward_key_event(keyval=keyval, keycode=0, state=0)
+        else:
+            logging.debug("Committing...")
+            # Delaying to partially solve the synchronization issue.
+            # Mostly for wine apps and for Gtk apps
+            # when the above check doesn't work.
+            time.sleep(0.005)
+            self.engine.commit_text(IBus.Text.new_from_string(string_to_commit))
+
+    def do_process_key_event(self):
+        # Check surrounding text capability after the first
+        # keypress.
+        if not self.ever_checked_surrounding_text and \
+                len(self.engine.raw_string) == 1:
+            self.ever_checked_surrounding_text = True
+            self.can_do_surrounding_text = self.check_surrounding_text()
+
+    def check_surrounding_text(self):
+        length = len(self.engine.prev_string)
+
+        ibus_text, cursor_pos, anchor_pos = self.engine.get_surrounding_text()
+        surrounding_text = ibus_text.get_text()
+
+        if surrounding_text.endswith(self.engine.prev_string):
+            self.delete_surrounding_text(offset=-length, nchars=length)
+
+            ibus_text, _, _ = self.engine.get_surrounding_text()
+            surrounding_text = ibus_text.get_text()
+
+            if not surrounding_text.endswith(self.engine.prev_string):
+                self.commit_text(
+                    IBus.Text.new_from_string(self.engine.prev_string))
+                return True
+            else:
+                return False
+
+    def do_focus_in(self):
+        self.focus_tracker.on_focus_changed()
+
+    def delete_prev_chars_with_backspaces(self, count):
+        if self.first_time_sending_backspace and \
+                (self.focus_tracker.is_in_firefox() or
+                 self.focus_tracker.is_in_chrome()):
+            # Sending a dead space key to dismiss the
+            # autocomplete box in Firefox and Chrome's
+            # address bar. See:
+            # https://github.com/BoGoEngine/ibus-bogo-python/pull/109
+            logging.debug("Dismissing autocomplete...")
+
+            self.engine.forward_key_event(IBus.space, 0, 0)
+            self.engine.forward_key_event(IBus.BackSpace, 14, 0)
+
+            self.first_time_sending_backspace = False
+
+        for i in range(count):
+            self.engine.forward_key_event(IBus.BackSpace, 14, 0)
+
+    def delete_prev_chars(self, count):
+        if count > 0:
+            if self.can_do_surrounding_text and \
+                    not self.focus_tracker.is_in_chrome():
+                logging.debug("Deleting surrounding text...")
+                self.engine.delete_surrounding_text(offset=-count, nchars=count)
+            else:
+                logging.debug("Sending backspace...")
+                self.delete_prev_chars_with_backspaces(count)
+
+
 class Engine(IBus.Engine):
     __gtype_name__ = 'EngineBoGo'
 
     def __init__(self, config, abbr_expander):
         super(Engine, self).__init__()
+
         self.config = config
-        self.input_context_capabilities = 0
         self.ui_delegate = UiDelegate(engine=self)
-
+        self.backend = BackspaceBackend(engine=self)
         self.abbr_expander = abbr_expander
-
-        self.can_do_surrounding_text = False
-        self.ever_checked_surrounding_text = False
-        self.reset()
 
         # Create a new thread to detect mouse clicks
         mouse_detector = MouseDetector.get_instance()
         mouse_detector.add_mouse_click_listener(self.reset)
 
-        self.focus_tracker = FocusTracker()
+        self.input_context_capabilities = 0
+        self.reset()
 
     def reset(self):
         self.new_string = ""
         self.prev_string = ""
         self.raw_string = ""
-        self.first_time_sending_backspace = True
+        self.backend.reset()
 
     # The "do_" part denotes a default signal handler
     def do_process_key_event(self, keyval, keycode, modifiers):
@@ -87,8 +219,8 @@ class Engine(IBus.Engine):
 
         This function gets called whenever a key is pressed.
         """
-        if self.focus_tracker.is_in_unity_dash():
-            return False
+        # if self.focus_tracker.is_in_unity_dash():
+        #     return False
 
         # Ignore key release events
         event_is_key_press = (modifiers & (1 << 30)) == 0
@@ -102,13 +234,6 @@ class Engine(IBus.Engine):
 
         if keyval in [IBus.Return, IBus.BackSpace, IBus.space]:
             return self.on_special_key_pressed(keyval)
-
-        # Check surrounding text capability after the first
-        # keypress.
-        if not self.ever_checked_surrounding_text and \
-                len(self.raw_string) == 1:
-            self.ever_checked_surrounding_text = True
-            self.can_do_surrounding_text = self.check_surrounding_text()
 
         if self.is_processable_key(keyval, modifiers):
             logging.debug("Key pressed: %c", chr(keyval))
@@ -136,7 +261,7 @@ class Engine(IBus.Engine):
 
             logging.debug("New string: %s", self.new_string)
 
-            self.commit_result(self.new_string)
+            self.backend.update_composition(self.new_string)
             self.prev_string = self.new_string
             return True
 
@@ -162,65 +287,6 @@ class Engine(IBus.Engine):
     #     logging.debug("Reset signal")
     #     self.reset()
 
-    def commit_result(self, string):
-        same_initial_chars = list(takewhile(lambda tupl: tupl[0] == tupl[1],
-                                            zip(self.prev_string,
-                                                self.new_string)))
-
-        n_backspace = len(self.prev_string) - len(same_initial_chars)
-        string_to_commit = self.new_string[len(same_initial_chars):]
-
-        self.delete_prev_chars(n_backspace)
-
-        # Charset conversion
-        # We encode a Unicode string into a byte sequence with the specified
-        # encoding and then re-interpret it as a Unicode string containing
-        # only latin-1 (ISO-8859-1) characters.
-        #
-        # This method has the flaw of not being able to commit raw legacy
-        # encoding strings but it's the only thing we can do now as we cannot
-        # commit bytes in Python 3, just Unicode string. Consider this example:
-        #
-        #     - UTF-8 'à': 0xc3 0xa0
-        #     - TCVN3 'à': 0xb5 (rendered as '¶')
-        #     - UTF-8 '¶': 0xc2 0xb6
-        #
-        # So if we typed 'à' in TCVN3, what the client receives would be
-        # 0xc2 0xb6. Field testing shows that this does not affect LibreOffice
-        # Writer and Kate (when forcing the file's encoding to be latin-1)
-        # though.
-        if self.config['output-charset'] != 'utf-8':
-            string_to_commit = string_to_commit \
-                .encode(self.config['output-charset']) \
-                .decode('latin-1')
-
-        # We cannot commit the text in Gtk since there is a bug in which
-        # sometimes committed text comes before the forwarded
-        # backspaces, resulting in undesirable output. Instead, we forward
-        # each character to the current input context.
-        #
-        # We also need to detect whether the current input context is from a
-        # GTK app by checking for the SURROUNDING_TEXT capability. It works
-        # because as of right now (2013/02/22, IBus 1.4.1), only the IBus Gtk
-        # client can do surrounding text.
-        #
-        # Very very veryyyy CRUDE, by the way.
-        if self.input_context_capabilities & IBus.Capabilite.SURROUNDING_TEXT:
-            logging.debug("Forwarding as commit...")
-
-            for ch in string_to_commit:
-                keyval = mapping[ch] if ch in mapping else ord(ch)
-                self.forward_key_event(keyval=keyval, keycode=0, state=0)
-        else:
-            logging.debug("Committing...")
-            # Delaying to partially solve the synchronization issue.
-            # Mostly for wine apps and for Gtk apps
-            # when the above check doesn't work.
-            time.sleep(0.005)
-            self.commit_text(IBus.Text.new_from_string(string_to_commit))
-
-        self.current_shown_text = string
-
     def is_processable_key(self, keyval, state):
         # This can be extended to support typing in French, Japanese,...
         # keyboards. But currently not working.
@@ -235,52 +301,6 @@ class Engine(IBus.Engine):
                      IBus.ModifierType.MOD1_MASK) == 0 and \
             (key.isalpha() or key in current_im.keys())
 
-    def check_surrounding_text(self):
-        length = len(self.prev_string)
-
-        ibus_text, cursor_pos, anchor_pos = self.get_surrounding_text()
-        surrounding_text = ibus_text.get_text()
-
-        if surrounding_text.endswith(self.prev_string):
-            self.delete_surrounding_text(offset=-length, nchars=length)
-
-            ibus_text, cursor_pos, anchor_pos = self.get_surrounding_text()
-            surrounding_text = ibus_text.get_text()
-
-            if not surrounding_text.endswith(self.prev_string):
-                self.commit_text(IBus.Text.new_from_string(self.prev_string))
-                return True
-            else:
-                return False
-
-    def delete_prev_chars_with_backspaces(self, count):
-        if self.first_time_sending_backspace and \
-                (self.focus_tracker.is_in_firefox() or
-                 self.focus_tracker.is_in_chrome()):
-            # Sending a dead space key to dismiss the
-            # autocomplete box in Firefox and Chrome's
-            # address bar. See:
-            # https://github.com/BoGoEngine/ibus-bogo-python/pull/109
-            logging.debug("Dismissing autocomplete...")
-
-            self.forward_key_event(IBus.space, 0, 0)
-            self.forward_key_event(IBus.BackSpace, 14, 0)
-
-            self.first_time_sending_backspace = False
-
-        for i in range(count):
-            self.forward_key_event(IBus.BackSpace, 14, 0)
-
-    def delete_prev_chars(self, count):
-        if count > 0:
-            if self.can_do_surrounding_text and \
-                    not self.focus_tracker.is_in_chrome():
-                logging.debug("Deleting surrounding text...")
-                self.delete_surrounding_text(offset=-count, nchars=count)
-            else:
-                logging.debug("Sending backspace...")
-                self.delete_prev_chars_with_backspaces(count)
-
     def do_enable(self):
         self.ui_delegate.do_enable()
 
@@ -288,7 +308,7 @@ class Engine(IBus.Engine):
         self.reset()
 
     def do_focus_in(self):
-        self.focus_tracker.on_focus_changed()
+        self.backend.do_focus_in()
 
     def do_focus_out(self):
         self.reset()
@@ -311,7 +331,6 @@ class Engine(IBus.Engine):
 
             deleted_char = self.new_string[-1]
             self.new_string = self.new_string[:-1]
-            self.current_shown_text = self.new_string
             self.prev_string = self.new_string
 
             if len(self.new_string) == 0:
@@ -329,13 +348,15 @@ class Engine(IBus.Engine):
                 expanded_string = self.abbr_expander.expand(self.prev_string)
 
                 if expanded_string != self.prev_string:
-                    self.commit_result(expanded_string)
+                    self.new_string = expanded_string
+                    self.commit_composition()
                     self.reset()
                     return False
 
             if self.config['skip-non-vietnamese'] and \
                     not bogo.validation.is_valid_string(self.prev_string):
-                self.commit_result(self.raw_string)
+                self.new_string = self.raw_string
+                self.commit_composition()
 
             self.reset()
             return False
