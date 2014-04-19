854a1a7795c6b4da60cf41cdbe625a5d9fe6ff3d
Trung Ngo <ndtrung4419@gmail.com>
2014-04-19 20:38:38 +0700

    Make config argument mandatory for AbbreviationExpander constructor

--- ibus_engine/abbr.py
+++ ibus_engine/abbr.py
@@ -3,17 +3,28 @@ import json
 
 
 class AbbreviationExpander():
+    """
+    An abbreviation/shorthand expander.
+    """
 
-    def __init__(self, config=None):
-        if config:
-            self.config = config
-        else:
-            self.config = {
-                "auto-capitalize-expansion": False
-            }
+    def __init__(self, config):
+        """
+        Args:
+            config: A dictionary-like object that has to contain a boolean
+                key "auto-capitalize-expansion" that instructs the expander
+                whether to capitalize the expanded string.
+                (i.e. Cm -> Con meo and CM -> CON MEO)
+        """
+        self.config = config
         self.abbr_rules = {}
 
     def watch_file(self, file_path):
+        """Watch a rule file for changes.
+
+        Args:
+            file_path: the path to a file containing expansion rules. See
+                read_file().
+        """
         self.read_file(file_path)
 
         # Setup automatic refreshing
@@ -24,6 +35,14 @@ class AbbreviationExpander():
         self.monitor.connect("changed", self.on_file_changed)
 
     def read_file(self, file_path):
+        """Read expansion rules from a file.
+
+        Args:
+            file_path: the path to the file containing the rules.
+
+        The rule file should be a JSON file containing a dictionary of
+        abbreviations and their expansions.
+        """
         try:
             with open(file_path) as f:
                 json_content = f.read()
@@ -36,9 +55,17 @@ class AbbreviationExpander():
             self.read_file(watched_file.get_path())
 
     def add_rule(self, abbreviated_string, full_string):
+        """Add an abbreviation rule.
+
+        Args:
+            abbreviated_string: the abbreviated string. E.g. 'vn'
+            full_string: the expanded string. E.g. 'Vietnam'
+        """
         self.abbr_rules[abbreviated_string] = full_string
 
     def expand(self, abbr_word):
+        """Expand an abbreviated word if possible.
+        """
         if self.config["auto-capitalize-expansion"]:
             abbr_word_lower = abbr_word.lower()
 
@@ -53,9 +80,8 @@ class AbbreviationExpander():
                 return expanded_word
             # if a lower case match is not found then
             # fallback to a normal case match
-        
+
         if abbr_word in self.abbr_rules:
             return self.abbr_rules[abbr_word]
         else:
             return abbr_word
-
