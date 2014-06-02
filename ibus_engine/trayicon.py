a490e8590d5ae346b583d481ae213dc200f1f720
Trung Ngo <ndtrung4419@gmail.com>
2014-06-02 22:55:09 +0700

    Fix icon path for the custom tray icon

--- ibus_engine/trayicon.py
+++ ibus_engine/trayicon.py
@@ -1,5 +1,12 @@
 # vim: set expandtab softtabstop=4 shiftwidth=4:
 from gi.repository import Gtk
+import os
+
+ENGINE_PATH = os.path.dirname(__file__)
+
+# FIXME: hardcoded path
+ICON_PATH = ENGINE_PATH + '/data/ibus-bogo-dev.svg'
+
 
 class TrayIcon:
     def __init__(self):
@@ -10,11 +17,10 @@ class TrayIcon:
         self.icon.set_from_icon_name("ibus")
 
     def enable(self):
-        self.icon.set_from_icon_name("ibus-bogo")
+        self.icon.set_from_file(ICON_PATH)
 
     def hide(self):
         self.icon.set_visible(False)
 
     def show(self):
         self.icon.set_visible(True)
-
