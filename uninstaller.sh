5326ef72601618723f72ab6f07d7c2a90b8ab147
Trung Ngo <ndtrung4419@gmail.com>
2014-07-17 02:55:26 +0700

    Remove some unnecessary files

--- uninstaller.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE=~/.local/share/ibus-bogo
-
-rm -rf $BASE
-rm -rf ~/.local/share/applications/ibus-bogo-setup.desktop
-gksudo "rm -rf /usr/share/ibus/component/bogo.xml"
-ibus-daemon --xim --daemonize --replace
